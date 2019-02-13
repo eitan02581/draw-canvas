e3b198f4323009aea044ca8914e6c59c871263e3
Eitan Elnekave <eitan02581@gmail.com>
2019-02-13 16:57:08 +0200

    start drawing

--- main.js
+++ main.js
@@ -1,28 +1,73 @@
 'use strict'
 var gCanvas
 var gCtx
+var gType
+var gMousePressed = false;
+var gLastX, gLastY;
 
 function init() {
     gCanvas = document.querySelector('#draw-canvas');
     gCtx = gCanvas.getContext('2d')
+    gType = 'pen'
     initCanvas()
 }
 
 function initCanvas() {
     gCanvas.width = document.body.scrollWidth
     gCanvas.height = document.body.scrollHeight
+    setMouseEvent()
+}
+
+function setMouseEvent() {
+    $('#draw-canvas').mousedown(function (e) {
+        gMousePressed = true;
+        draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#draw-canvas').mousemove(function (e) {
+        if (gMousePressed) {
+            draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#draw-canvas').mouseup(function (e) {
+        gMousePressed = false;
+    });
+    $('#draw-canvas').mouseleave(function (e) {
+        gMousePressed = false;
+    });
 }
 
 function onSelect(elSelect) {
     var selectedVal = elSelect.value
-    if (selectedVal === 'circle') drawCircle()
-    if (selectedVal === 'pen') drawPen()
+    if (selectedVal === 'circle') gType = 'circle'
+    if (selectedVal === 'square') gType = 'square'
+    if (selectedVal === 'pen') gType = 'pen'
 
 }
 
 // draws options
-function drawPen() {
+function draw(x, y, isDown) {
+    if (gType === 'pen') drawPen(x, y, isDown)
+    if (gType === 'square') drawSquare(x, y, isDown)
+    if (gType === 'circle') drawCircle(x, y, isDown)
 
+    gLastX = x;
+    gLastY = y;
+}
+
+
+function drawPen(x, y, isDown) {
+    if (isDown) {
+        gCtx.beginPath();
+        gCtx.strokeStyle = $('#selColor').val();
+        gCtx.lineWidth = $('#selWidth').val();
+        gCtx.lineJoin = "round";
+        gCtx.moveTo(gLastX, gLastY);
+        gCtx.lineTo(x, y);
+        gCtx.closePath();
+        gCtx.stroke();
+    }
 }
 
 function drawCircle() {
@@ -30,19 +75,4 @@ function drawCircle() {
     gCtx.arc(100, 300, 50, 0, 2 * Math.PI);
     gCtx.stroke()
     gCtx.fill()
-}
-
-
-function canvasClicked() {
-    draw()
-}
-
-function draw() {
-    gCtx.beginPath();
-    gCtx.moveTo(prevX, prevY);
-    gCtx.lineTo(currX, currY);
-    gCtx.strokeStyle = x;
-    gCtx.lineWidth = y;
-    gCtx.stroke();
-    gCtx.closePath();
 }
