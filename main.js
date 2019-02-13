8fcffdc23b50f251f459850a0a8bff508bcff6fd
Eitan Elnekave <eitan02581@gmail.com>
2019-02-13 18:32:21 +0200

    for pull

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function init() {
 }
 
 function initCanvas() {
-    gCanvas.width = document.body.scrollWidth
-    gCanvas.height = document.body.scrollHeight
+    gCanvas.width = 600;
+    gCanvas.height = 300
     setMouseEvent()
 }
 
@@ -43,6 +43,7 @@ function onSelect(elSelect) {
     if (selectedVal === 'circle') gType = 'circle'
     if (selectedVal === 'square') gType = 'square'
     if (selectedVal === 'pen') gType = 'pen'
+    if (selectedVal === 'tube') gType = 'tube'
 
 }
 
@@ -51,6 +52,7 @@ function draw(x, y, isDown) {
     if (gType === 'pen') drawPen(x, y, isDown)
     if (gType === 'square') drawSquare(x, y, isDown)
     if (gType === 'circle') drawCircle(x, y, isDown)
+    if (gType === 'tube') drawTube(x, y, isDown)
 
     gLastX = x;
     gLastY = y;
@@ -70,9 +72,63 @@ function drawPen(x, y, isDown) {
     }
 }
 
-function drawCircle() {
-    gCtx.beginPath()
-    gCtx.arc(100, 300, 50, 0, 2 * Math.PI);
-    gCtx.stroke()
-    gCtx.fill()
+function drawCircle(x, y, isDown) {
+    if (isDown) {
+        gCtx.beginPath()
+        gCtx.strokeStyle = $('#selColor').val();
+        gCtx.arc(x, y, 50, 0, 2 * Math.PI);
+        gCtx.moveTo(gLastX, gLastY);
+        gCtx.stroke()
+    }
+}
+
+function drawTube(x, y, isDown) {
+    if (isDown) {
+        gCtx.beginPath()
+        gCtx.strokeStyle = $('#selColor').val();
+        gCtx.arc(x, y, 50, 0, 2 * Math.PI);
+        gCtx.moveTo(gLastX, gLastY);
+        gCtx.lineTo(x, y);
+        gCtx.stroke()
+    }
+}
+
+function onClear() {
+    initCanvas()
+}
+
+function onSave(elLink) {
+    var imgContent = gCanvas.toDataURL('image/jpg');
+    elLink.href = imgContent
+}
+
+
+
+function renderCanvas(img) {
+    gCanvas.width = img.width;
+    gCanvas.height = img.height;
+    gCtx.drawImage(img, 0, 0);
+}
+
+function onFileInputChange(ev) {
+    handleImageFromInput(ev, renderCanvas)
+}
+
+
+
+
+//UPLOAD IMG WITH INPUT FILE
+function handleImageFromInput(ev, onImageReady) {
+    var reader = new FileReader();
+
+    reader.onload = function (ev) {
+        var img = new Image();
+        img.onload = function () {
+            gCanvas.width = img.width;
+            gCanvas.height = img.height;
+            gCtx.drawImage(img, 0, 0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(ev.target.files[0]);
 }
